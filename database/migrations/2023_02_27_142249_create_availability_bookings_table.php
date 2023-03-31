2d1a4bf27763be3f8536b63ed6450ee98f967cb3
WanderSmits <wandersmits@gmail.com>
2023-03-31 15:11:05 +0200

    revert renaming migration

--- database/migrations/2023_02_27_142249_create_availability_bookings_table.php
+++ database/migrations/2023_02_27_142249_create_availability_bookings_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('accommodation_bookings', function (Blueprint $table) {
+        Schema::create('availability_bookings', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('accommodation_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('availability_id')->constrained()->cascadeOnDelete();
             $table->foreignId('booking_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('accommodation_bookings');
+        Schema::dropIfExists('availability_bookings');
     }
 };
